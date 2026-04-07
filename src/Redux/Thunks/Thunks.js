8c223cb661d356d2ad146be3fcdee0bf4421264b
ayush <ayush349919@gmail.com>
2026-04-07 18:08:33 +0530

    done

--- src/Redux/Thunks/Thunks.js
+++ src/Redux/Thunks/Thunks.js
@@ -3,7 +3,6 @@ import axios from "axios";
 
 export const getProducts = createAsyncThunk("products/getProducts", async () => {
     const response = await axios.get('https://dummyjson.com/products')
-    localStorage.setItem('products', JSON.stringify(response.data.products))
     console.log(response);
     return response.data.products;
 
